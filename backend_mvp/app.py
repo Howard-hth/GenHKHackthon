cb32a77e0ee04648695f36578df95a1e7cb73268
Howard-hth <tinghaokang@gmail.com>
2025-01-05 16:31:46 +0800

    Update app.py

--- backend_mvp/app.py
+++ backend_mvp/app.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from controllers.api_controller import api
 from helpers.scrape import scraper
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def index():
